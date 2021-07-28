801290934a97acb76866726dd89ee13b2d291e92
yandutka@mail.ru <yandutka@mail.ru>
2021-07-28 18:50:46 +0300

    commit

--- Function/Main.cpp
+++ Function/Main.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
+п»ї#include<iostream>
 using namespace std;
 
-int Add(int a, int b);  //Прототип функции (Объявление функции - Function declaration)
+int Add(int a, int b);  //РџСЂРѕС‚РѕС‚РёРї С„СѓРЅРєС†РёРё (РћР±СЉСЏРІР»РµРЅРёРµ С„СѓРЅРєС†РёРё - Function declaration)
 int Sub(int a, int b);
 int Mul(int a, int b);
 double Div(int a, int b);
@@ -10,32 +10,32 @@ void main()
 {
 	setlocale(LC_ALL, "Rus");
 	int a, b;
-	cout << "Введите два числа: "; cin >> a >> b;
-	int c = Add(a, b);  //Вызов или использование функции (Function call)
+	cout << "Р’РІРµРґРёС‚Рµ РґРІР° С‡РёСЃР»Р°: "; cin >> a >> b;
+	int c = Add(a, b);  //Р’С‹Р·РѕРІ РёР»Рё РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ С„СѓРЅРєС†РёРё (Function call)
 	cout << a << " + " << b << " = " << c << endl;
 	cout << a << " - " << b << " = " << Sub(a,b) << endl;
 	cout << a << " * " << b << " = " << Mul(a,b) << endl;
 	cout << a << " / " << b << " = " << Div(a,b) << endl;
 
 }
-int Add(int a, int b)  //Реализация функции (Определение функции - Function defenition)
+int Add(int a, int b)  //Р РµР°Р»РёР·Р°С†РёСЏ С„СѓРЅРєС†РёРё (РћРїСЂРµРґРµР»РµРЅРёРµ С„СѓРЅРєС†РёРё - Function defenition)
 {
-	//Addition - Сложение
+	//Addition - РЎР»РѕР¶РµРЅРёРµ
 	int c = a + b;
 	return c;
 }
 int Sub(int a, int b)
 {
-	//Subtraction - Вычитание
+	//Subtraction - Р’С‹С‡РёС‚Р°РЅРёРµ
 	return a - b;
 }
 int Mul(int a, int b)
 {
-	//Multiplication - Умножение
+	//Multiplication - РЈРјРЅРѕР¶РµРЅРёРµ
 	return a * b;
 }
 double Div(int a, int b)
 {
-	//Division - Деление
+	//Division - Р”РµР»РµРЅРёРµ
 	return (double)a / b;
 }
